f130fcab4a98a53e2c308b7e2a27f2eab61752b8
Dylmarcor <dylancorbett@icloud.com>
2020-04-11 11:52:35 -0700

    add fav byte string

--- xor_fav_byte.py
+++ xor_fav_byte.py
@@ -1,4 +1,9 @@
 # Convert hex to an int
 fav = int("73626960647f6b206821204f21254f7d694f7624662065622127234f726927756d", 16)
+fav_list = "".split(fav)
+
+for i in fav_list:
+    if i != 0 || i != 1
+    fav.remove(i)
 
 print(fav)
